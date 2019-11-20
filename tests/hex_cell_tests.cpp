eb79cde241367286a3a646c187be59d1071bc6f1
estelle0500 <estelle0500@gmail.com>
2019-11-19 22:37:28 -0600

    Tested calculation of hexagon vertices

---
+++ tests/hex_cell_tests.cpp
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+#include "hex_cell.h"
+
+TEST_CASE("Test HexCell constructor creates correct vertices in a hexagon") {
+    vector<ofVec3f> expected_vertices = {{40, 20}, {30, 37.32}, {10, 37.32},
+                                         {0, 20},  {10, 2.68}, {30, 2.68}};
+    
+	HexCell cell(ofVec3f(20, 20, 0));
+	vector<ofVec3f> vertices = cell.GetVertices();
+    REQUIRE(vertices.size() == 6);
+    
+	for (int i = 0; i < 6; i++) {
+        REQUIRE(vertices[i].x == Approx(expected_vertices[i].x).epsilon(0.01));
+		REQUIRE(vertices[i].y == Approx(expected_vertices[i].y).epsilon(0.01));
+    }
+}
