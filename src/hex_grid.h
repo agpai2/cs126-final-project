102abb2611c29afa81d8b968bafc1f47b766acc4
estelle0500 <estelle0500@gmail.com>
2019-11-20 10:24:34 -0600

    Created HexGrid class

---
+++ src/hex_grid.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ofMain.h"
+#include "hex_cell.h"
+
+const size_t kGridSize = 5;
+
+class HexGrid {
+public:
+    HexGrid();
+    HexGrid(ofVec3f center);
+	vector<HexCell> GetCells();
+
+	void setup();
+    void update();
+    void draw();
+    void exit();
+
+private:
+    ofVec3f center_;
+    vector<HexCell> cells_;
+};
