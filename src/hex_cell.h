897099a4d69d06c1397ccc692819d86dfe120c3c
estelle0500 <estelle0500@gmail.com>
2019-11-19 20:02:15 -0600

    Declared geometry methods in HexCell class

---
+++ src/hex_cell.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ofMain.h"
+
+constexpr float kHexCellRadius = 20.0;
+constexpr float kHexCellAngleDeg = 60.0;
+
+class HexCell {
+   public:
+    HexCell(ofVec3f center);
+    vector<ofVec3f> GetVertices();
+
+   private:
+    ofVec3f center_;
+    ofVec3f right_vertex_;
+    ofVec3f upper_right_vertex_;
+    ofVec3f upper_left_vertex_;
+    ofVec3f left_vertex_;
+    ofVec3f lower_left_vertex_;
+    ofVec3f lower_right_vertex_;
+};
