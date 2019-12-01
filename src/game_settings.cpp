a1aac10b656ad75e99bce6629f3776855926553c
estelle0500 <estelle0500@gmail.com>
2019-12-01 16:53:31 -0600

    Fixed empty files

---
+++ src/game_settings.cpp
@@ -0,0 +1,14 @@
+#include "game_settings.h"
+        
+namespace Hexplosions {
+GameSettings::GameSettings(GameGridSize grid_size, size_t num_players) : 
+    grid_size_(grid_size), num_players_(num_players) {}
+
+size_t GameSettings::GetGridSize() { 
+    return grid_size_;
+}
+
+size_t GameSettings::GetNumPlayers() {
+    return num_players_;
+}
+}  // namespace Hexplosions
---
+++ src/game_settings.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace Hexplosions {
+/**
+ * @enum    GameGridSize
+ *
+ * @brief   Length (number of cells) of the game grid
+ *         
+ */
+enum GameGridSize {
+    SMALL = 5, 
+    MEDIUM = 7,
+    LARGE = 9
+};
+
+constexpr size_t kMinimumPlayers = 2;
+constexpr size_t kMaximumPlayers = 4;
+
+/**
+ * @class   GameSettings
+ *
+ * @brief   Game settings that can be modified by the user before the game starts.
+ *
+ * @author  Estelle Lee
+ * @date    1/12/2019
+ */
+
+class GameSettings {
+  public:
+    GameSettings(GameGridSize grid_size, size_t num_players);
+    size_t GetGridSize();
+    size_t GetNumPlayers();
+
+  private:
+    GameGridSize grid_size_;
+    size_t num_players_;
+};
+}
